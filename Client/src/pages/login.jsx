89875fd5764ccb9f5a3e2595468eb2199f9a568a
kuldeep <kuldeepsinghchouhan224@gmail.com>
2025-07-22 00:09:53 +0530

    design_changes done

--- Client/src/pages/login.jsx
+++ Client/src/pages/login.jsx
@@ -192,7 +192,7 @@ const LoginPage = () => {
           <button
             type="button"
             onClick={handleSubmit}
-            className="w-full py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition-colors disabled:bg-blue-400 disabled:cursor-not-allowed"
+            className="w-full py-3 bg-blue-600 text-grey  font-bold rounded-lg hover:bg-blue-700 transition-colors disabled:bg-blue-400 disabled:cursor-not-allowed"
             disabled={isSubmitting}
             aria-busy={isSubmitting}
           >
@@ -214,7 +214,7 @@ const LoginPage = () => {
           </p>
           Developed by Codes Book
           <div className="flex justify-center space-x-4 mt-2">
-            <a
+            {/*<a
               href="https://github.com/prasoonj17"
               target="_blank"
               rel="noopener noreferrer"
@@ -230,7 +230,7 @@ const LoginPage = () => {
               >
                 <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
               </svg>
-            </a>
+            </a>*/}
             <a
               href="https://www.linkedin.com/in/prasoonjain/"
               target="_blank"
